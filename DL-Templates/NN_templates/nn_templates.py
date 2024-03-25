2a0bea1eb268b62c7128e708c5795ddd3ae89022
Dilshan N. Wickramarachchi <58634673+dilshan-n-wickramarachchi@users.noreply.github.com>
2024-03-25 15:26:55 +0000

    Signed-off-by: Dilshan N. Wickramarachchi <58634673+dilshan-n-wickramarachchi@users.noreply.github.com>

---
+++ DL-Templates/NN_templates/nn_templates.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 15:22:27 2024
+
+title: NN template
+description: Create a pytorch template for neural network
+
+@author: wickramw
+@github: https://github.com/dilshan-n-wickramarachchi
+"""
+
+# Import Libraries
+import torch 
